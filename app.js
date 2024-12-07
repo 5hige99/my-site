ff26820e8ae9764c43acbf8b457fe2330b23c818
5hige99 <shige99dev@gmail.com>
2024-12-08 00:02:47 +0900

    init

---
+++ app.js
@@ -0,0 +1,3 @@
+document.querySelector('.avatar img').src = './icon.png';
+document.querySelector('.name').innerText = 'shige | Shigeta Tsubasa';
+document.querySelector('.title').innerText = 'Software Engineer';
