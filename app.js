4283ed40c96715d0a5c8e0e10e6db0d5d64053b4
5hige99 <shige99dev@gmail.com>
2024-12-08 22:17:11 +0900

    delete

--- app.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.avatar img').src = './icon.png';
-document.querySelector('.name').innerText = 'shige | Shigeta Tsubasa';
-document.querySelector('.title').innerText = 'Software Engineer';
